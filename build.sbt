d5f1fe5a4de6fd5038e4aea0134e1c03df9eb0dd
Vincent Jousse <vincent@jousse.org>
2012-01-21 16:56:35 +0100

    Use specs2 instead of scalatest

--- build.sbt
+++ build.sbt
@@ -3,3 +3,10 @@ name := "VjousseBot"
 version := "0.1"
 
 scalaVersion := "2.9.1"
+
+libraryDependencies ++= Seq(
+  "org.specs2" %% "specs2" % "1.7.1" % "test"
+  )
+
+resolvers ++= Seq("snapshots" at "http://scala-tools.org/repo-snapshots",
+  "releases"  at "http://scala-tools.org/repo-releases")
--- src/test/scala/antwar/BreadthFirstSearchTest.scala
+++ src/test/scala/antwar/BreadthFirstSearchTest.scala
@@ -3,28 +3,31 @@ package test
 
 import sandbox._
 
-import org.scalatest._
+import org.specs2.mutable._
 
-class BreadthSarchTest extends FunSuite {
+class BreadthSarchTest extends Specification {
 
-  test("Find closest ant to Food 3,3") {
+  "My BFS algorithm" should {
+    "Find the closest ant" in {
 
-    val myAnt = new MyAnt(Tile(7, 1))
-    val myOtherAnt = new MyAnt(Tile(9, 9))
+      val myAnt = new MyAnt(Tile(7, 1))
+      val myOtherAnt = new MyAnt(Tile(9, 9))
 
-    val food = new Food(Tile(3, 3))
-    val board = new Board(
-      10, 10,
-      myAnts = Map(myAnt.tile -> myAnt, myOtherAnt.tile -> myOtherAnt),
-      food = Map(food.tile -> food)
-    )
+      val food = new Food(Tile(3, 3))
+      val board = new Board(
+        10, 10,
+        myAnts = Map(myAnt.tile -> myAnt, myOtherAnt.tile -> myOtherAnt),
+        food = Map(food.tile -> food)
+        )
 
-    val search = new BreadthFirstSearch(board)
+      val search = new BreadthFirstSearch(board)
 
-    def isAnt(tile: Tile): Boolean = board.myAnts.contains(tile)
+      def isAnt(tile: Tile): Boolean = board.myAnts.contains(tile)
 
-    val antFound = search.findTile(food.tile, isAnt)
+      val antFound = search.findTile(food.tile, isAnt)
 
-    assert(antFound === Some(myAnt))
+      antFound must beSome(myAnt)
+    }
   }
+
 }
