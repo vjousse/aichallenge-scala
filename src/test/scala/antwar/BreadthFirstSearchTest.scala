f6ce24f03658a6b81ea0449d7c67574c9d751112
Vincent Jousse <vincent@jousse.org>
2012-01-21 15:02:50 +0100

    Add some fixtures

--- src/test/scala/antwar/BreadthFirstSearchTest.scala
+++ src/test/scala/antwar/BreadthFirstSearchTest.scala
@@ -1,8 +1,23 @@
 package antwar
 package test
 
+import sandbox._
+
 import org.scalatest._
 
 class BreadthSarchTest extends FunSuite {
 
+  test("Find closest ant to Food 3,3") {
+
+    val myAnt = new MyAnt(Tile(7, 1))
+    val myOtherAnt = new MyAnt(Tile(9, 9))
+
+    val food = new Food(Tile(3, 3))
+    val board = new Board(
+      10, 10,
+      myAnts = Map(myAnt.tile -> myAnt, myOtherAnt.tile -> myOtherAnt),
+      food = Map(food.tile -> food)
+    )
+
+  }
 }
