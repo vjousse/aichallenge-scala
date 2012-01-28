238d7cb1d446f08b8998664c1aa0c3d9c559a824
Vincent Jousse <vincent@jousse.org>
2012-01-28 16:51:08 +0100

    Add level to the nodes

--- src/main/scala/sandbox/BreadthFirstSearch.scala
+++ src/main/scala/sandbox/BreadthFirstSearch.scala
@@ -6,16 +6,17 @@ import scala.util.control.Exception._
 
 case class BreadthFirstSearch(board: Board, goal: Tile => Boolean) {
 
-  case class Node(value: Tile, children: List[Node] = Nil, parent: Option[Node] = None) {
+  case class Node(value: Tile, level: Int, children: List[Node] = Nil, parent: Option[Node] = None) {
     override def toString = "Tree(" + value.toString + " {" + children.map(_.toString).mkString(",") + "})\n"
   }
 
   /**
    *  Build a graph for the BFS, with a deep limit to levelLimit
    */
-  def buildGraph(tile: Tile, marked: Set[Tile] = Set(), currentLevel: Int = 0, levelLimit: Int = 3): Node = {
-
-    Node(tile,
+  def buildGraph(tile: Tile, marked: Set[Tile] = Set(), currentLevel: Int = 0, levelLimit: Int = 10): Node =
+    Node(
+      tile,
+      currentLevel,
       if (currentLevel > levelLimit)
         Nil
       else {
@@ -23,20 +24,17 @@ case class BreadthFirstSearch(board: Board, goal: Tile => Boolean) {
         neighbors map { n => buildGraph(n, marked ++ neighbors, currentLevel + 1, levelLimit) }
       })
 
-  }
-
   private def searchGraph(queue: Queue[Node]): Option[Tile] = catching(classOf[NoSuchElementException]) opt queue.dequeue match {
       case Some((node, rest)) =>
         if (goal(node.value))
           Some(node.value)
         else
           searchGraph(rest.enqueue(node.children))
-
       case _ => None
     }
 
   def findTile(startTile: Tile): Option[Tile] = {
-    val graph = buildGraph(startTile)
+    val graph = buildGraph(startTile, Set(startTile))
     searchGraph(Queue(graph))
   }
 
